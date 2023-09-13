a651480b4ce85a70c301136a818963cd71ad1f8b
Joshua Cheng <joshuacheng888@gmail.com>
2023-09-12 19:52:31 -0700

    Initial Commit

---
+++ RedPack/RedPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedPackApp.swift
+//  RedPack
+//
+//  Created by Joshua Cheng on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RedPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
